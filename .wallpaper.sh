d9cb67ab512526b6666c92243e5a149e3d4dd0c6
andrzej3393 <andrzej3393@gmail.com>
2015-09-03 11:35:32 +0200

    global upgrade

--- .wallpaper.sh
+++ .wallpaper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-WALLPAPERS_DIR="/home/andrzej3393/Pictures/wallpapers/"
+WALLPAPERS_DIR="/home/andrzej3393/Images/wallpapers/"
 feh --bg-fill "$WALLPAPERS_DIR`ls $WALLPAPERS_DIR | shuf -n 1`"
